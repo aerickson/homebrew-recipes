dba56de0ba4369fa60c9cbb5fd658e63692295bc
Andrew Erickson <aje@cloudscaling.com>
2013-09-08 14:31:17 -0700

    adding tripwire recipe
    - in progress… buggy due to env and
      compiler requirements.

---
+++ tripwire.rb
@@ -0,0 +1,30 @@
+require 'formula'
+
+# needs to be installed with: 
+#  brew install tripwire --env=std --use-llvm
+
+class Tripwire < Formula
+  homepage 'http://sourceforge.net/projects/tripwire/'
+  url 'http://sourceforge.net/projects/tripwire/files/tripwire-src/tripwire-2.4.2.2/tripwire-2.4.2.2-src.tar.bz2/download'
+  sha1 '6fd59dfb7071841f1e255b62ae121d4d065faee7'
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    # janky install
+
+    # this will fail due to interactive license part...
+    #system "make", "install"
+  end
+
+  fails_with :clang do
+    build 425
+    cause <<-EOS.undent
+      Install via 'brew install tripwire --env=std --use-llvm' due to bad
+      interaction with Homebrew's super root and clang.
+      EOS
+  end
+
+  test do
+    system "tripwire"
+  end
+end
