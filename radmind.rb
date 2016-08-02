75b7dc50c8cad457f5676d683848525009ecc989
Andrew Erickson <aerickson@gmail.com>
2016-08-02 00:04:09 -0700

    upgrade radmind to 1.14.0
    - set a sha256 also

--- radmind.rb
+++ radmind.rb
@@ -1,17 +1,21 @@
 require 'formula'
 
 class Radmind < Formula
+  version '1.14.0'
   homepage 'http://rsug.itd.umich.edu/software/radmind/'
-  url 'https://sourceforge.net/projects/radmind/files/radmind/radmind-1.13.0/radmind-1.13.0.tar.gz/download'
-  sha1 '73adae4771422b282ca044aef5002884951ed081'
-  version '1.13.0'
+  url "https://sourceforge.net/projects/radmind/files/radmind/radmind-#{version}/radmind-#{version}.tar.gz/download"
+  sha256 '653fa852554b5bf5bb7982068b7891732b99956afc3edfde4fa8249ff8e3a5f8'
+
+  depends_on "openssl"
 
   def install
+    ENV.append "LDFLAGS", '-L/usr/local/opt/openssl/lib'
+    ENV.append "CPPFLAGS", '-I/usr/local/opt/openssl/include'
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
+              "--with-openssl=#{Formula["openssl"].opt_prefix}",
                           "--prefix=#{prefix}"
     # fails here due to superenv... selects 'cc' vs 'gcc'
     # fails the first time, works the second!?!
-    system "make ; true"
     system "make"
     system "make", "install"
   end
