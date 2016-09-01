e18e94b517704804c5d1f675f3de34db0df849cb
Andrew J. Erickson <andy@bolt.me>
2016-09-01 11:35:21 -0700

    add hacked up terraform
    so that i can install head and release versions at once

---
+++ terraform-head.rb
@@ -0,0 +1,104 @@
+require "language/go"
+
+class TerraformHead < Formula
+  desc "Tool to build, change, and version infrastructure"
+  homepage "https://www.terraform.io/"
+  url "https://github.com/hashicorp/terraform/archive/v0.7.1.tar.gz"
+  sha256 "b6a6b5553755ebbf5362e8104eea801234c127831f5c12792a55fbbc8792ef1d"
+  head "https://github.com/hashicorp/terraform.git"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9f0e4d7336f6ebca000fc5a98cfe49ef819e5d70f797083b04172e6d26c46378" => :el_capitan
+    sha256 "1152b3771ff6b6976d758df9ff3bf8e44320d692955c27c357964db71296b911" => :yosemite
+    sha256 "b00d8a313c32ea91952acbea5c7820b8790f3ef58c688ad690e3b88b9fce801b" => :mavericks
+  end
+
+  depends_on "go" => :build
+
+  terraform_deps = %w[
+    github.com/mitchellh/gox 770c39f64e66797aa46b70ea953ff57d41658e40
+    github.com/mitchellh/iochan 87b45ffd0e9581375c491fef3d32130bb15c5bd7
+  ]
+
+  terraform_deps.each_slice(2) do |x, y|
+    go_resource x do
+      url "https://#{x}.git", :revision => y
+    end
+  end
+
+  go_resource "golang.org/x/tools" do
+    url "https://go.googlesource.com/tools.git", :revision => "977844c7af2aa555048a19d28e9fe6c392e7b8e9"
+  end
+
+  def install
+    ENV["GOPATH"] = buildpath
+    # For the gox buildtool used by terraform, which doesn't need to
+    # get installed permanently
+    ENV.append_path "PATH", buildpath
+
+    terrapath = buildpath/"src/github.com/hashicorp/terraform"
+    terrapath.install Dir["*"]
+    Language::Go.stage_deps resources, buildpath/"src"
+
+    cd "src/github.com/mitchellh/gox" do
+      system "go", "build"
+      buildpath.install "gox"
+    end
+
+    cd "src/golang.org/x/tools/cmd/stringer" do
+      system "go", "build"
+      buildpath.install "stringer"
+    end
+
+    cd terrapath do
+      # v0.6.12 - source contains tests which fail if these environment variables are set locally.
+      ENV.delete "AWS_ACCESS_KEY"
+      ENV.delete "AWS_SECRET_KEY"
+
+      terraform_files = `go list ./...`.lines.map { |f| f.strip unless f.include? "/vendor/" }.compact
+      system "go", "test", *terraform_files
+
+      mkdir "bin"
+      arch = MacOS.prefer_64_bit? ? "amd64" : "386"
+      system "gox",
+        "-arch", arch,
+        "-os", "darwin",
+        "-output", "bin/terraform-{{.Dir}}", *terraform_files
+      bin.install "bin/terraform-terraform" => "terraform-head"
+#      zsh_completion.install "contrib/zsh-completion/_terraform"
+    end
+  end
+
+  test do
+    minimal = testpath/"minimal.tf"
+    minimal.write <<-EOS.undent
+      variable "aws_region" {
+          default = "us-west-2"
+      }
+
+      variable "aws_amis" {
+          default = {
+              eu-west-1 = "ami-b1cf19c6"
+              us-east-1 = "ami-de7ab6b6"
+              us-west-1 = "ami-3f75767a"
+              us-west-2 = "ami-21f78e11"
+          }
+      }
+
+      # Specify the provider and access details
+      provider "aws" {
+          access_key = "this_is_a_fake_access"
+          secret_key = "this_is_a_fake_secret"
+          region = "${var.aws_region}"
+      }
+
+      resource "aws_instance" "web" {
+        instance_type = "m1.small"
+        ami = "${lookup(var.aws_amis, var.aws_region)}"
+        count = 4
+      }
+    EOS
+    system "#{bin}/terraform", "graph", testpath
+  end
+end
